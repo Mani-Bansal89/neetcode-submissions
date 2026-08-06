782662bf3d14144d3354e068358b2377500568b5
Mani-Bansal89 <115208623+Mani-Bansal89@users.noreply.github.com>
2026-08-05 18:30:03 -0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s)%2==1:
+            return False
+        open_brackets = []
+        brackets_dict = {'(':')','{':'}','[':']'}
+        for bracket in s:
+            if bracket in brackets_dict:
+                open_brackets.append(bracket)
+            elif open_brackets:
+                open_bracket = open_brackets.pop()
+                close_bracket = brackets_dict[open_bracket]
+                if close_bracket!=bracket:
+                    return False
+            else:
+                return False
+        if len(open_brackets)!=0:
+            return False
+        return True
