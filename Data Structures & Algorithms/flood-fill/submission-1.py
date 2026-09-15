8fddb1cc83ee7830410580d9ddfb76c6c7a5938d
Mani-Bansal89 <115208623+Mani-Bansal89@users.noreply.github.com>
2026-09-14 22:16:35 -0700

    Add: flood-fill - submission-1

---
+++ Data Structures & Algorithms/flood-fill/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        queue = deque()
+        n = len(image)
+        m = len(image[0])
+        
+        og_color = image[sr][sc]
+        visited = {}
+        queue.append((sr, sc))
+        image[sr][sc] = color
+        visited[(sr,sc)] = True
+        
+        while queue:
+            curr = queue.popleft()
+            sr = curr[0]
+            sc = curr[1]
+            
+            for (i, j) in [(0, 1),(0,-1),(1,0),(-1,0)]:
+                    if 0 <= sr+i < n and 0 <= sc+j < m and image[sr+i][sc+j] == og_color and (sr + i, sc + j) not in visited:
+                        queue.append((sr + i, sc + j))
+                        image[sr + i][sc + j] = color
+                        visited[(sr + i,sc + j)] = True
+
+        return image 
+
+
+        
